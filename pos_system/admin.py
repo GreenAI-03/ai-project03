5481ebf0c63635520b3c15a465560fdcc96c389b
AndrewLPT <familysbox305@gmail.com>
2024-08-15 04:25:48 +0800

    0815:新增sales類別(尚未設定SQL)

--- pos_system/admin.py
+++ pos_system/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Sale
 
 class ProductInline(admin.TabularInline):
     model = Product
@@ -16,3 +16,18 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'description')
     list_filter = ('category',)
     search_fields = ('name', 'description')
+
+
+from django.contrib import admin
+from .models import Sale  # 確保導入您的模型
+
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'quantity', 'unit_price', 'sale_date', 'total_price')
+    list_filter = ('sale_date',)
+    search_fields = ('product_name',)
+
+admin.site.register(Sale, SaleAdmin)
+
+
+
+
