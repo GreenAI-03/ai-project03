f44fdfe6b0b0439cbab44af8f7446290b5bfed2f
AndrewLPT <familysbox305@gmail.com>
2024-08-15 06:44:58 +0800

    新增Custromer類別:基本資料,電子載具,特殊喜好,消費時輸入載具會顯示最近五筆消費紀錄等等

--- pos_system/admin.py
+++ pos_system/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product, Sale
+from .models import Category, Product, Sale, Customer
 
 class ProductInline(admin.TabularInline):
     model = Product
@@ -16,18 +16,17 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'description')
     list_filter = ('category',)
     search_fields = ('name', 'description')
-
-
-from django.contrib import admin
-from .models import Sale  # 確保導入您的模型
-
+    
 class SaleAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'quantity', 'unit_price', 'sale_date', 'total_price')
-    list_filter = ('sale_date',)
-    search_fields = ('product_name',)
-
+    list_display = ('product_class', 'product_name', 'quantity', 'unit_price', 'sale_date', 'total_price', )
+    list_filter = ('sale_date', 'product_class', 'product_name', ) 
+    search_fields = ('product_name', 'sale_date', 'product_class',  'unit_price',)
+    
 admin.site.register(Sale, SaleAdmin)
 
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_number', 'e_invoice_carrier', 'created_at')
+    search_fields = ('e_invoice_carrier', 'name', 'email', 'phone_number')
+    list_filter = ('created_at','phone_number', 'e_invoice_carrier',)
 
-
-
+admin.site.register(Customer, CustomerAdmin)
