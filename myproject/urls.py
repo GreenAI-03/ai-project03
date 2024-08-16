d9dff6a7ca00f322e8dbb47a26c061d695099e1a
Loon404 <james840929@gmail.com>
2024-08-16 16:14:26 +0800

    改login頁面

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from pos_system.views import add_to_cart
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pos_system.urls')),
-    path('add_to_cart/', add_to_cart)
+    path('add_to_cart/', add_to_cart),
 ]
 
